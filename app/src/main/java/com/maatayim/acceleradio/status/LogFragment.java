d71aa401588d5bcd01fd65b88d6b820e9d86986e
Vova <vovax87@gmail.com>
2019-02-03 22:26:34 +0200

    added array for adapter

--- app/src/main/java/com/maatayim/acceleradio/status/LogFragment.java
+++ app/src/main/java/com/maatayim/acceleradio/status/LogFragment.java
@@ -23,58 +23,60 @@ import static com.maatayim.acceleradio.Prefs.ATTRIBUTE_STATUS_TIME;
 
 public class LogFragment extends Fragment {
 
-	private static SimpleAdapter sAdapter;
+    private static SimpleAdapter sAdapter;
+    private static boolean isActionDowm;
+    private static ArrayList<Map<String, String>> statusMessages;
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.log_fragment, container, false);
-		initStatusDataMessages(view);
-		return view;
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.log_fragment, container, false);
+        initStatusDataMessages(view);
+        return view;
+    }
 
-	private void initStatusDataMessages(View view) {
-		ArrayList<Map<String, String>> statusMessages = Prefs.getStatusMessages();
-
-		sAdapter = new SimpleAdapter(getActivity(), statusMessages, R.layout.log_item_view ,Prefs.getFrom(), Prefs.getToMessages());
-		ListView lvSimple = (ListView) view.findViewById(R.id.statusListView);
-		lvSimple.setAdapter(sAdapter);
-		lvSimple.setSelection(statusMessages.size()-1);
-		lvSimple.setOnTouchListener(new View.OnTouchListener() {
+    private void initStatusDataMessages(View view) {
+        statusMessages = new ArrayList<>(Prefs.getStatusMessages());
+        sAdapter = new SimpleAdapter(getActivity(), statusMessages, R.layout.log_item_view, Prefs.getFrom(), Prefs.getToMessages());
+        ListView lvSimple = (ListView) view.findViewById(R.id.statusListView);
+        lvSimple.setAdapter(sAdapter);
+        lvSimple.setSelection(statusMessages.size() - 1);
+        lvSimple.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    isActionDowm = true;
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    isActionDowm = false;
+                }
                 return false;
             }
         });
-	}
 
-	public static void notifyChanges(){
-		ArrayList<Map<String, String>> statusMessages = Prefs.getStatusMessages();
-		if (statusMessages != null){
-			Collections.sort(statusMessages, new ValueComparator());
-		}
+    }
 
+    public static void notifyChanges() {
+        // dont notify change is on clicked
+        if (isActionDowm) return;
 
-		if (sAdapter != null){
-			Handler handler = new Handler(Looper.getMainLooper());
-			handler.post(new Runnable() {
-				@Override
-				public void run() {
-					sAdapter.notifyDataSetChanged();
-				}
-			});
-		}
-	}
+        statusMessages = new ArrayList<>(Prefs.getStatusMessages());
+        Collections.sort(statusMessages, new ValueComparator());
 
-	static class ValueComparator implements Comparator<Map<String, String>> {
+        if (sAdapter != null) {
+            sAdapter.notifyDataSetChanged();
+
+        }
+    }
+
+    static class ValueComparator implements Comparator<Map<String, String>> {
 
 
-		// Note: this comparator imposes orderings that are inconsistent with
-		// equals.
+        // Note: this comparator imposes orderings that are inconsistent with
+        // equals.
 
-		@Override
-		public int compare(Map<String, String> o1, Map<String, String> o2) {
-			return o1.get(ATTRIBUTE_STATUS_TIME).compareToIgnoreCase(o2.get(ATTRIBUTE_STATUS_TIME))*-1;
-		}
-	}
+        @Override
+        public int compare(Map<String, String> o1, Map<String, String> o2) {
+            return o1.get(ATTRIBUTE_STATUS_TIME).compareToIgnoreCase(o2.get(ATTRIBUTE_STATUS_TIME));
+        }
+    }
 }
