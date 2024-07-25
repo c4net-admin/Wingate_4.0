ff6aa049447901d0655fd801cee6c056eb55e58b
Shani Hilkiya <shani@200apps.co>
2024-07-25 15:13:03 +0300

    update deprecated location API

--- app/src/main/java/com/maatayim/acceleradio/FusedLocationService.java
+++ app/src/main/java/com/maatayim/acceleradio/FusedLocationService.java
@@ -1,113 +1,91 @@
 package com.maatayim.acceleradio;
 
-
-
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
-import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.FusedLocationProviderApi;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
 
-public class FusedLocationService implements
-LocationListener,
-GoogleApiClient.ConnectionCallbacks,
-GoogleApiClient.OnConnectionFailedListener {
+public class FusedLocationService {
 	private static final long INTERVAL = 1000 * 10;
 	private static final long FASTEST_INTERVAL = 1000 * 5;
 	private static final long ONE_MIN = 1000 * 60;
 	private static final long REFRESH_TIME = ONE_MIN * 5;
-//	private static final float MINIMUM_ACCURACY = 50.0f;
-	private static final String TAG = "200apps.AcceleRadio.FusedLocationService";
+	private static final String TAG = "FusedLocationService";
 
-	private LocationRequest locationRequest;
-	private GoogleApiClient googleApiClient;
-	private Location location;
-	private FusedLocationProviderApi fusedLocationProviderApi = LocationServices.FusedLocationApi;
-	private LocationListener locationListener;
+	private final FusedLocationProviderClient fusedLocationClient;
+	private final LocationRequest locationRequest;
+	private final LocationCallback locationCallback;
+	private final LocationListener locationListener;
+	private final Context context;
 
+	private Location lastLocation;
 	private boolean isListening = false;
-	private boolean isConnected = false;
+
 
 	public FusedLocationService(Context context, LocationListener locationListener) {
-		locationRequest = LocationRequest.create();
-		locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-		locationRequest.setInterval(INTERVAL);
-		locationRequest.setFastestInterval(FASTEST_INTERVAL);
-
+		this.context = context;
 		this.locationListener = locationListener;
+		fusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
 
-		googleApiClient = new GoogleApiClient.Builder(context)
-					.addApi(LocationServices.API)
-					.addConnectionCallbacks(this)
-					.addOnConnectionFailedListener(this)
-					.build();
+		locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, INTERVAL)
+				.setMinUpdateIntervalMillis(FASTEST_INTERVAL)
+				.build();
 
-		if (googleApiClient != null) {
-			googleApiClient.connect();
-		}
-		googleApiClient.registerConnectionFailedListener(this);
+		locationCallback = new LocationCallback() {
+			@Override
+			public void onLocationResult(@NonNull LocationResult locationResult) {
+				for (Location location : locationResult.getLocations()) {
+					onLocationChanged(location);
+				}
+			}
+		};
 	}
 
-	@Override
-	public void onConnected(Bundle connectionHint) {
-		Log.d(TAG, "onConnected ");
-		Location currentLocation = fusedLocationProviderApi.getLastLocation(googleApiClient);
-		fusedLocationProviderApi.requestLocationUpdates(googleApiClient, locationRequest, this);
-		isListening = true;
-		isConnected = true;
-		if (currentLocation != null && currentLocation.getTime() > REFRESH_TIME) {
-			location = currentLocation;
-			locationListener.onLocationChanged(location);
-		}
-
-	}
-
-
-
-	@Override
-	public void onLocationChanged(Location location) {
-//		Log.d(TAG, "onLocationChanged ");
+	private void onLocationChanged(Location location) {
+		Log.d(TAG, "onLocationChanged");
+		lastLocation = location;
 		locationListener.onLocationChanged(location);
-		this.location = location;
 	}
 
+	public void startListening() {
+		if (!isListening) {
+			if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+					&& ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+				Log.e(TAG, "Location permission not granted");
+				return;
+			}
+			fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+			isListening = true;
+
+			fusedLocationClient.getLastLocation().addOnSuccessListener(location -> {
+				if (location != null && System.currentTimeMillis() - location.getTime() < REFRESH_TIME) {
+					onLocationChanged(location);
+				}
+			});
+		}
+	}
+
+	public void stopListening() {
+		if (isListening) {
+			fusedLocationClient.removeLocationUpdates(locationCallback);
+			isListening = false;
+		}
+	}
 
 	public Location getLocation() {
-		return this.location;
+		return lastLocation;
 	}
-
-
-	@Override
-	public void onConnectionSuspended(int cause) {
-		Log.d(TAG, "onConnectionSuspended ");
-		isConnected = false;
-	}
-
-
-	@Override
-	public void onConnectionFailed(ConnectionResult connectionResult) {
-		Log.d(TAG, "onConnectionFailed ");
-	}
-
-	public void startListening(){
-		if(!isListening && isConnected){
-			fusedLocationProviderApi.requestLocationUpdates(googleApiClient, locationRequest, this);
-			isListening = true;
-		}
-
-	}
-	public void stopListening(){
-		if (fusedLocationProviderApi != null && googleApiClient != null && googleApiClient.isConnected()) {
-			fusedLocationProviderApi.removeLocationUpdates(googleApiClient,
-					FusedLocationService.this);
-		}
-		isListening = false;
-	}
-	
 }
